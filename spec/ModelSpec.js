32dc41c6db8222a4b8eb51e94248929bff50efaa
Marc Kennedy <marcthomaskennedy@gmail.com>
2017-05-20 15:01:23 +0100

    Add tests for Model.add()

--- spec/ModelSpec.js
+++ spec/ModelSpec.js
@@ -2,22 +2,34 @@ describe("Model", () => {
     let model;
 
     beforeAll(() => {
-        model = new Model();
+        mode = new Model();
     });
     describe("Test Model.init() method", () => {
         it("Should be of type Array", () => {
-            //const notes = JSON.parse(model.notes);
-                expect(notes.constructor).toBe(jasmine.any(Array));
+            let notes = JSON.parse(localStorage.notes);
+            expect(notes).toEqual(jasmine.any(Array));
             });
         it("Should be an empty Array", () => {
-            expect().toBe(0);
+            let notes = JSON.parse(localStorage.notes);
+            expect(notes.length).toBe(0);
         });
+        // Should be of type String before JSON.parse
         it("Should be of type Sting", () => {
-            expect(model.notes.constructor).toBe(jasmine.any(String));
+            expect(localStorage.notes).toEqual(jasmine.any(String));
         });
     });
     describe("Test Model.add() method", () => {
-
+        // Should throw a TypeError if argument is not of type Object
+        it("Should throw a TypeError", () => {
+            expect(() => {
+                model.add(1234)
+            }).toThrow(TypeError);
+        });
+        it("Should be of length 1", () => {
+            model.add({content: "Hello, world!"});
+            let data = JSON.parse(localStorage.notes);
+            expect(data.length).toBe(1);
+        });
     });
     describe("Test Model.getAllNotes() method", () => {
 
