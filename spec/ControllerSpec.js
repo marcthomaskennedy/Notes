8b4c1c7d63284c07f2711e9db87581acda075e6c
Marc Kennedy <marcthomaskennedy@gmail.com>
2017-05-20 16:39:55 +0100

    Add a Model instance as an argument to Controller.getNotes() tests

--- spec/ControllerSpec.js
+++ spec/ControllerSpec.js
@@ -15,16 +15,16 @@ describe("Controller", () => {
     });
     describe("Test Controller.getNotes() method", () => {
         it("Should be an Array", () => {
-            const data = Controller.getNotes();
+            const data = Controller.getNotes(model);
             expect(data).toEqual(jasmine.any(Array));
         });
         it("Should be an empty Array", () => {
-            const data = Controller.getNotes();
+            const data = Controller.getNotes(model);
             expect(data.length).toBe(0);
         });
         it("Should be of length 1", () => {
             model.add({content: "Hello, Controller!"});
-            const data = Controller.getNotes();
+            const data = Controller.getNotes(model);
             expect(data.length).toBe(1);
         });
     });
